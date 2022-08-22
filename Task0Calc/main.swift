8e07a87390ecb7658b877c348f88f0c4d0377e5c
Alex <63662620+AlexFoxD@users.noreply.github.com>
2022-08-22 17:57:19 +0300

    console calc, task 0

--- Task0Calc/main.swift
+++ Task0Calc/main.swift
@@ -7,5 +7,51 @@
 
 import Foundation
 
-print("Hello, World!")
+var pointMenu: String?
+var a: String?
+var b: String?
 
+repeat {
+    print("Привет, это программа калькулятор! Выберите пункт меню: ")
+    print("1. Ввести числа для операций")
+    print("2. +")
+    print("3. -")
+    print("4. *")
+    print("5. /")
+    print("6. Выход")
+
+    pointMenu = readLine()
+    
+    if let pointMenu = Int(pointMenu!) {
+        switch pointMenu {
+        case 1:
+            repeat {
+                print("Введите первое число: ")
+                a = readLine()
+                print("Введите второе число: ")
+                b = readLine()
+            } while Int(a!) == nil || Int(b!) == nil
+            
+        case 2 where a != nil || b != nil:
+                /*if let a = Int(a!), let b =  Int(b!) {
+                    print("Результат операции \(a) + \(b) = " + String(a + b))
+                }*/
+                print("Результат операции \(a!) + \(b!) = " + String(Int(a!)! + Int(b!)!))
+
+        case 3 where a != nil || b != nil:
+                print("Результат операции \(a!) - \(b!) = " + String(Int(a!)! - Int(b!)!))
+            
+        case 4 where a != nil || b != nil:
+                print("Результат операции \(a!) * \(b!) = " + String(Int(a!)! * Int(b!)!))
+        
+        case 5 where a != nil || b != nil:
+            Int(b!)! != 0 ? print("Результат операции \(a!) / \(b!) = " + String(Int(a!)! / Int(b!)!)) : print("Делить \(Int(a!)!) на 0 нельзя!")
+            
+        case 6:
+            break
+            
+        default:
+            print("Неверный пункт меню!")
+        }
+    }
+} while Int(pointMenu!) != 6
